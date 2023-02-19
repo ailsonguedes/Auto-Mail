274dcf5630728d1bb885511b1347dc8dd4efa9e4
ailsonguedes <ailsonsixseven@gmail.com>
2023-02-19 19:03:32 -0300

    Att 190220231902 implementation of checkpoint func

--- autoMail.py
+++ autoMail.py
@@ -1,18 +1,21 @@
+import os
 import smtplib
 import email.message
 import pandas as pd
+import pickle
 
 senderName = str(input("Type your name: "))
 senderEmail = str(input("Type your email: "))
 senderPassword = str(input("Type your password: "))
 
-df = pd.read_excel(r'diretório completo para localizar o seu arquivo xlsx')
-df_test = df.head(1000)
+df = pd.read_excel(r'emailslist.xlsx path')
+df_test = df.head(200)
 
 addresseeName = (df_test['Nome'].to_list())
 addresseeEmail = (df_test['Email'].to_list())
 
 i = 0
+checkPoint = []
 
 for list in addresseeEmail:
 
@@ -20,6 +23,8 @@ for list in addresseeEmail:
 
     fromP = addresseeEmail[i]
     nameP = addresseeName[i]
+
+    checkPoint.append((addresseeName[i], addresseeEmail[i]))
   
     #<p></p>
     email_body = f"""
@@ -42,7 +47,22 @@ for list in addresseeEmail:
     # login Credentials for sending the mail
     s.login(msg['From'], password)
     s.sendmail(msg['From'], msg['To'], msg.as_string().encode('utf-8'))
+    
     print('Email enviado')
 
+  def save_checkpoint(checkPoint):
+      with open('path where you want to save your checkpoint.pkl', 'wb') as f:
+          pickle.dump(checkPoint, f)
+
   send_email()
+  save_checkpoint(checkPoint)
+
   i = i + 1
+
+def load_checkpoint():
+  with open('path where you saved your checkpoint.pkl', 'rb') as f:
+    safe_checkpoint = pickle.load(f)
+
+  print("Checkpoint Save List: ", safe_checkpoint)
+  
+load_checkpoint()  
